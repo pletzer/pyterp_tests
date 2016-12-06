d18ea4cd8e97e48d1f70cc4318cba097dd9de665
Alexander Pletzer <alexander@gokliya.net>
2016-12-06 14:58:32 +1300

    now running over many resolutions

---
+++ rotated_pole/run_node_interp.py
@@ -0,0 +1,83 @@
+from subprocess import call
+import re
+
+def getEvaluationTime(filename):
+	m = re.search(r'evaluation\s+([\d\.e\-]+)', open(filename, 'r').read())
+	if m:
+		return float(m.group(1))
+	return None
+
+def getWeightsTime(filename):
+	m = re.search(r'weights\s+([\d\.e\-]+)', open(filename, 'r').read())
+	if m:
+		return float(m.group(1))
+	return None
+
+def getNumberOfInvalidPoints(filename):
+	m = re.search(r'invalid points\:\s+(\d+)', open(filename, 'r').read())
+	if m:
+		return int(m.group(1))
+	return None
+
+
+src_celldims = [(10, 20),
+                (20, 40),
+                (40, 80),
+                (80, 160),
+                (160, 320)]
+
+
+ns = []
+libcf_eval = []
+libcf_weights = []
+esmf_eval = []
+esmf_weights = []
+for srcDims in src_celldims:
+	srcNodeDimsStr = '"{0},{1}"'.format(srcDims[0] + 1, srcDims[1] + 1)
+	dstNodeDimsStr = '"{0},{1}"'.format(srcDims[0]//2 + 1, srcDims[1]//2 + 1)
+
+	# generate the grids
+	dstDims = (srcDims[0]//2, srcDims[1]//2)
+	call(['python', 'generate_field.py', \
+		'--src_nj', '{}'.format(srcDims[0] + 1), \
+		'--src_ni', '{}'.format(srcDims[1] + 1), \
+		'--dst_nj', '{}'.format(dstDims[0] + 1), \
+		'--dst_ni', '{}'.format(dstDims[1] + 1), \
+		])
+
+	srcN = srcDims[0] * srcDims[1]
+	dstN = dstDims[0] * dstDims[1]
+	ns.append(srcN * dstN)
+	print('number of src * dst cells is {}'.format(srcN * dstN))
+
+	# run esmf
+	err = open('log.err', 'w')
+	out = open('log.txt', 'w')
+	call(['python', 'esmf_interp.py'], stdout=out, stderr=err)
+	out.close()
+	esmf_eval.append(getEvaluationTime('log.txt'))
+	esmf_weights.append(getWeightsTime('log.txt'))
+
+	# run libcf
+	err = open('log.err', 'w')
+	out = open('log.txt', 'w')
+	call(['python', 'libcf_interp.py'], stdout=out, stderr=err)
+	out.close()
+	libcf_eval.append(getEvaluationTime('log.txt'))
+	libcf_weights.append(getWeightsTime('log.txt'))
+	assert(getNumberOfInvalidPoints('log.txt') == 0)
+
+
+from matplotlib import pylab
+import matplotlib
+pylab.loglog(ns, esmf_eval, 'ro', markersize=8)
+pylab.loglog(ns, esmf_weights, 'rs', markersize=8)
+pylab.loglog(ns, libcf_eval, 'bo', markersize=8) 
+pylab.loglog(ns, libcf_weights, 'bs', markersize=8)
+pylab.legend(['esmf eval', 'esmf wgts', 'libcf eval', 'libcf wgts'], loc=2)
+pylab.plot(ns, libcf_eval, 'b--', ns, libcf_weights, 'b--', \
+	       ns, esmf_eval, 'r--', ns, esmf_weights, 'r--')
+pylab.xlabel('num src cells * num dst cells')
+pylab.ylabel('time [sec]')
+pylab.title('billinear interpolation')
+pylab.show()
--- rotated_pole/test_esmf_conserve.py
+++ rotated_pole/test_esmf_conserve.py
@@ -2,18 +2,35 @@ import ESMF
 import numpy
 
 def create2DLats(nodeDims, latAxis):
+    """
+    Create a 2d latitude coordinate from a 1d axis
+    @param nodeDims number of latitude/longitude nodes
+    @param latAxis 1D array of latitude values
+    @return 2D lat coordinate
+    """
     return numpy.outer(latAxis, numpy.ones((nodeDims[1],)))
 
 def create2DLons(nodeDims, lonAxis):
+    """
+    Create a 2d longitude coordinate from a 1d axis
+    @param nodeDims number of latitude/longitude nodes
+    @param lonAxis 1D array of longitude values
+    @return 2D lon coordinate
+    """
     return numpy.outer(numpy.ones((nodeDims[0],)), lonAxis)
 
-
 def createGrid(nodeDims, lats2D, lons2D):
-
+    """
+    Create a structured grid from coordinates
+    @param nodeDims number of latitude/longitude nodes
+    @param lats2D latitude coordinates (corner points)
+    @param lons2D langitude coordinates (corner points)
+    @return ESMF Grid
+    """
     cellDims = numpy.array(nodeDims) - 1
 
     grid = ESMF.Grid(cellDims,
-                     num_peri_dims=None, # number of periodic dimensions
+                     num_peri_dims=0, # number of periodic dimensions
                      periodic_dim=None, # 1, 2, or 3
                      pole_dim=None, # default is 1 (1-based indexing?). Not sure if this follows Fortran ordering
                      coord_sys=ESMF.api.constants.CoordSys.SPH_DEG, # other choices are CART, SPH_RAD
@@ -40,6 +57,13 @@ def createGrid(nodeDims, lats2D, lons2D):
     return grid
 
 def createField(grid, name, data):
+    """
+    Create cell-centered structured field
+    @param grid ESMF grid
+    @param name field name
+    @param data 2D data of size number of lat cells times number of lon cells
+    @return ESMF Field
+    """
     field = ESMF.Field(grid,
                        name=name,
                        typekind=ESMF.api.constants.TypeKind.R8,
@@ -48,24 +72,27 @@ def createField(grid, name, data):
     field.data[...] = data
     return field
 
-srcPointDims = (11, 21)
-dstPointDims = (6, 11)
+# set the src/dst grid dimensions
+srcPointDims = (10 + 1, 20 + 1) # (100 + 1, 200 + 1) works!
+dstPointDims = (5 + 1, 10 + 1)
 
 srcCellDims = (srcPointDims[0] - 1, srcPointDims[1] - 1)
 dstCellDims = (dstPointDims[0] - 1, dstPointDims[1] - 1)
 
-# 2d latitude and longitude coordinates (uniform grids)
+# 2d latitude and longitude coordinates (uniform grids for simplicity)
 srcLats2D = create2DLats(srcPointDims, numpy.linspace(-90., 90, srcPointDims[0]))
 srcLons2D = create2DLons(srcPointDims, numpy.linspace(-180., 180, srcPointDims[1]))
 dstLats2D = create2DLats(dstPointDims, numpy.linspace(-90., 90, dstPointDims[0]))
 dstLons2D = create2DLons(dstPointDims, numpy.linspace(-180., 180, dstPointDims[1]))
 
-# create the ESMF src/dst grid
+# create the ESMF src/dst grids
 srcGrid = createGrid(srcPointDims, srcLats2D, srcLons2D)
 dstGrid = createGrid(dstPointDims, dstLats2D, dstLons2D)
 
 # create the ESMF src/dst cell centered fields
-srcData = numpy.ones(srcCellDims, numpy.float64) # some arbitrary field
+srcLats2DCell = 0.25*(srcLats2D[:-1, :-1] + srcLats2D[1:, :-1] + srcLats2D[1:, 1:] + srcLats2D[:-1, 1:])
+srcLons2DCell = 0.25*(srcLons2D[:-1, :-1] + srcLons2D[1:, :-1] + srcLons2D[1:, 1:] + srcLons2D[:-1, 1:])
+srcData = srcLons2DCell # some arbitrary field
 srcField = createField(srcGrid, 'src', srcData)
 dstData = numpy.zeros(dstCellDims, numpy.float64) # initializing the dst field to zero
 dstField = createField(dstGrid, 'dst', dstData)
@@ -78,8 +105,6 @@ regrid = ESMF.Regrid(srcfield=srcField, dstfield=dstField,
 regrid(srcField, dstField)
 
 print(dstField.data)
-
-# clean up
 
 
 
