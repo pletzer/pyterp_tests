6f7a6bf237533eeea41ed609f9931a6f2e63b375
Alex Pletzer <alexander@gokliya.net>
2016-12-08 07:57:30 +1300

    adding corner and center data

--- rotated_pole/esmf_conserve.py
+++ rotated_pole/esmf_conserve.py
@@ -54,19 +54,19 @@ def createData(filename, prefix):
 
     latIndex, lonIndex = 0, 1
     cellDims = numpy.array([latsPoint.shape[0] - 1, latsPoint.shape[1] - 1])
-    grid = ESMF.Grid(max_index=cellDims) #, num_peri_dims=1, periodic_dim=1)
+    #grid = ESMF.Grid(max_index=cellDims, num_peri_dims=1, periodic_dim=0) # periodic_dim=0 for longitudes
+    stgrs = [ESMF.StaggerLoc.CORNER, ESMF.StaggerLoc.CENTER]
+    grid = ESMF.Grid(max_index=cellDims, num_peri_dims=1, staggerloc=stgrs)
 
-    grid.add_coords(staggerloc=ESMF.StaggerLoc.CORNER, coord_dim=latIndex)
-    grid.add_coords(staggerloc=ESMF.StaggerLoc.CORNER, coord_dim=lonIndex)
+    # do we need to add those?
+    #grid.add_coords(staggerloc=ESMF.StaggerLoc.CORNER, coord_dim=latIndex)
+    #grid.add_coords(staggerloc=ESMF.StaggerLoc.CORNER, coord_dim=lonIndex)
 
     #grid.add_coords(staggerloc=ESMF.StaggerLoc.CENTER, coord_dim=latIndex)
     #grid.add_coords(staggerloc=ESMF.StaggerLoc.CENTER, coord_dim=lonIndex)
 
     coordLatPoint = grid.get_coords(coord_dim=latIndex, staggerloc=ESMF.StaggerLoc.CORNER)
     coordLonPoint = grid.get_coords(coord_dim=lonIndex, staggerloc=ESMF.StaggerLoc.CORNER)
-
-    #coordLatCell = grid.get_coords(coord_dim=latIndex, staggerloc=ESMF.StaggerLoc.CENTER)
-    #coordLonCell = grid.get_coords(coord_dim=lonIndex, staggerloc=ESMF.StaggerLoc.CENTER)
 
     # get the local start/end index sets and set the point coordinates
     iBegLat = grid.lower_bounds[ESMF.StaggerLoc.CORNER][latIndex]
@@ -76,19 +76,21 @@ def createData(filename, prefix):
     coordLatPoint[...] = latsPoint[iBegLat:iEndLat, iBegLon:iEndLon]
     coordLonPoint[...] = lonsPoint[iBegLat:iEndLat, iBegLon:iEndLon]
 
-    nodeDims = (iEndLat - iBegLat, iEndLon - iBegLon)
+    coordLatCell = grid.get_coords(coord_dim=latIndex, staggerloc=ESMF.StaggerLoc.CENTER)
+    coordLonCell = grid.get_coords(coord_dim=lonIndex, staggerloc=ESMF.StaggerLoc.CENTER)
 
-    # get the local start/end index sets and set the cell coordinates
-    #iBegLat = grid.lower_bounds[ESMF.StaggerLoc.CENTER][latIndex]
-    #iEndLat = grid.upper_bounds[ESMF.StaggerLoc.CENTER][latIndex]
-    #iBegLon = grid.lower_bounds[ESMF.StaggerLoc.CENTER][lonIndex]
-    #iEndLon = grid.upper_bounds[ESMF.StaggerLoc.CENTER][lonIndex]
-    #coordLatCell[...] = latsCell[iBegLat:iEndLat, iBegLon:iEndLon]
-    #coordLonCell[...] = lonsCell[iBegLat:iEndLat, iBegLon:iEndLon]
+    iBegLat = grid.lower_bounds[ESMF.StaggerLoc.CENTER][latIndex]
+    iEndLat = grid.upper_bounds[ESMF.StaggerLoc.CENTER][latIndex]
+    iBegLon = grid.lower_bounds[ESMF.StaggerLoc.CENTER][lonIndex]
+    iEndLon = grid.upper_bounds[ESMF.StaggerLoc.CENTER][lonIndex]
+    coordLatCell[...] = latsCell[iBegLat:iEndLat, iBegLon:iEndLon]
+    coordLonCell[...] = lonsCell[iBegLat:iEndLat, iBegLon:iEndLon]
+
+    nodeDims = (iEndLat - iBegLat, iEndLon - iBegLon)
 
     # create and set the field
     field = ESMF.Field(grid, name="air_temperature", 
-                       staggerloc=ESMF.StaggerLoc.CENTER)
+                       meshloc=ESMF.StaggerLoc.CENTER)
     field.data[...] = cubeCell.data[:]
 
     return grid, field, nodeDims
@@ -104,18 +106,14 @@ dstGrid, dstData, dstNodeDims = createData(dst_file, b"dst")
 # save the reference (exact) field data
 dstDataRef = dstData.data.copy()
 dstData.data[...] = -1
+print('ooo dstData.data = {}'.format(dstData.data))
 
 # compute the interpolation weights
 tic = time.time()
 regrid = ESMF.api.regrid.Regrid(srcData, dstData,
-                                src_mask_values=None, dst_mask_values=None,
+                                src_mask_values=numpy.array([0]), dst_mask_values=numpy.array([0]),
                                 regrid_method=ESMF.api.constants.RegridMethod.CONSERVE,
-                                pole_method=None,
-                                regrid_pole_npoints=None, # only relevant if method is ALLAVG
-                                line_type=ESMF.api.constants.LineType.GREAT_CIRCLE, # how the distance between two points is computed
-                                norm_type=None, # only for conservative regridding
                                 unmapped_action=ESMF.api.constants.UnmappedAction.IGNORE, 
-                                ignore_degenerate=True, # produce an error if two points are degenerate and if set to False
                                 src_frac_field=None, dst_frac_field=None)
 timeStats['weights'] = time.time() - tic
 
@@ -124,7 +122,7 @@ tic = time.time()
 #print('ooo dstDataRef = {}'.format(dstDataRef))
 print('--- dstData.data = {}'.format(dstData.data))
 regrid(srcData, dstData)
-#print('+++ dstData.data = {}'.format(dstData.data))
+print('+++ dstData.data = {}'.format(dstData.data))
 
 timeStats['evaluation'] = time.time() - tic
 
