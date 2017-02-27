a1df7a20d6e656e53010884ca02bed1c3ae389d7
Alexander Pletzer <alexander@gokliya.net>
2017-02-27 14:31:48 +1300

    ~ sigrid_conserve now runs

---
+++ uniform/sigrid_conserve.py
@@ -0,0 +1,98 @@
+from __future__ import print_function
+import sigrid.conserveInterp2D
+import iris
+import numpy
+import sys
+import argparse
+from functools import reduce
+import time
+
+parser = argparse.ArgumentParser(description='Conservatively interpolate using sigrid')
+parser.add_argument('--src_file', type=str, dest='src_file', default='src.nc',
+                    help='Source data file name')
+parser.add_argument('--dst_file', type=str, dest='dst_file', default='dst.nc',
+                    help='Destination data file name')
+parser.add_argument('--plot', dest='plot', action='store_true', help='Plot')
+
+args = parser.parse_args()
+
+if args.src_file is '':
+    print('ERROR: must provide source data file name')
+    parser.print_help()
+    sys.exit(1)
+
+if args.dst_file is '':
+    print('ERROR: must provide destination data file name')
+    parser.print_help()
+    sys.exit(1)
+
+src_file = args.src_file.encode('UTF-8') # python3
+dst_file = args.dst_file.encode('UTF-8') # python3
+ndims = 2
+
+def createData(filename, prefix):
+    # use iris to read in the data
+    pointCube = iris.load(filename, iris.Constraint(cube_func = lambda c: c.var_name == 'pointData'))[0]
+    cellCube = iris.load(filename, iris.Constraint(cube_func = lambda c: c.var_name == 'cellData'))[0]
+    coordsPoint = pointCube.coords()
+    lats1d = coordsPoint[0].points
+    lons1d = coordsPoint[1].points
+    lats = numpy.outer(lats1d, numpy.ones(lons1d.shape, lons1d.dtype))
+    lons = numpy.outer(numpy.ones(lats1d.shape, lats1d.dtype), lons1d)
+    data = cellCube.data
+
+    return lats, lons, data
+    
+
+timeStats = {
+    'weights': float('nan'),
+    'evaluation': float('nan'),
+}
+
+srcLatsCoords, srcLonsCoords, srcData = createData(src_file, b"src")
+dstLatsCoords, dstLonsCoords, dstData = createData(dst_file, b"dst")
+
+# save the reference (exact) field data
+dstDataRef = dstData.copy()
+
+# compute the interpolation weights
+tic = time.time()
+interp = sigrid.conserveInterp2D.ConserveInterp2D()
+
+interp.setDstGrid(dstLatsCoords, dstLonsCoords)
+periodicity = (False, True) # NEED TO CHECK PERIODICITY
+interp.setSrcGrid(periodicity, srcLatsCoords, srcLonsCoords)
+interp.computeWeights()
+timeStats['weights'] = time.time() - tic
+
+# interpolate
+tic = time.time()
+dstData = interp.apply(srcData)
+timeStats['evaluation'] = time.time() - tic
+
+# compute error
+srcNtot = len(srcData.flat)
+dstNtot = len(dstData.flat)
+error =  numpy.sum(abs(dstData - dstDataRef)) / float(dstNtot)
+print('sigrid interpolation:')
+print('\tsrc: {} ntot: {}'.format(srcLatsCoords.shape, srcNtot))
+print('\tdst: {} ntot: {}'.format(dstLatsCoords.shape, dstNtot))
+print('interpolation error: {:.3g}'.format(error))
+totTime = 0.0
+print('time stats:')
+for k, v in timeStats.items():
+    print('\t{0:<32} {1:>.3g} sec'.format(k, v))
+    totTime += v
+print('\t{0:<32} {1:>.3g} sec'.format('total', totTime))
+
+# check sum
+checksum = numpy.sum(dstData, axis=None)
+print('check sum: {:.15g}'.format(checksum))
+
+# plot
+if args.plot:
+    from matplotlib import pylab
+    latsCell = 0.25 * (dstLatsCoords[0:-1, 0:-1] + dstLatsCoords[1:, 0:-1] + dstLatsCoords[1:, 1:] + dstLatsCoords[0:-1, 1:])
+    lonsCell = 0.25 * (dstLonsCoords[0:-1, 0:-1] + dstLonsCoords[1:, 0:-1] + dstLonsCoords[1:, 1:] + dstLonsCoords[0:-1, 1:])
+    pylab.pcolor(lonsCell, latsCell, dstData)
+    pylab.show()
